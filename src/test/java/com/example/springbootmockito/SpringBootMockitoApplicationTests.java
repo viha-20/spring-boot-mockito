23813e039ef24d5067e4d8f062b1939d25e046f7
V.Jayasekara <108672553+viha-20@users.noreply.github.com>
2024-06-23 19:19:53 +0530

    Creating Spring Boot Mockito Project

---
+++ src/test/java/com/example/springbootmockito/SpringBootMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
